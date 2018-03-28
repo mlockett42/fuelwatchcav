ffd1cb61bf55e5f7cea1d02578757e1df90e4f68
Mark Lockett <mlockett@bigpond.com>
2018-03-28 18:28:17 +0800

    Add skeleton front end

---
+++ front-end/fuelwatch/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import, unicode_literals, print_function
+try:
+    import js
+except ImportError:
+    js = None
+from cavorite import c, t, Router, callbacks, timeouts, ajaxget
+from .mainscreen import mainscreen_view
+
+def start():
+    callbacks.initialise_global_callbacks()
+    timeouts.initialise_timeout_callbacks()
+    ajaxget.initialise_ajaxget_callbacks()
+    codemirror.initialise_codemirror_callbacks()
+    body = js.globals.document.body
+
+    error_404_page = c("div", [c("p", "No match 404 error"),
+                               c("p", [c("a", {"href": "/#!"}, "Back to main page")])])
+
+
+    r = Router({r'^$':  mainscreen_view(),
+                },
+                error_404_page, body)
+    r.route()
+
---
+++ front-end/fuelwatch/mainscreen.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import, print_function
+from cavorite.HTML import *
+
+
+def mainscreen_view():
+    return div([
+                 p("Fuel watch"),
+               ])
