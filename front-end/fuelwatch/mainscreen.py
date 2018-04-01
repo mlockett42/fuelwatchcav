38615b7088ad04e6794cc6b3faf892cbcb4d00ae
Mark Lockett <mlockett@bigpond.com>
2018-04-01 16:37:36 +0800

    Add more ui options for the fuel search screen

--- front-end/fuelwatch/mainscreen.py
+++ front-end/fuelwatch/mainscreen.py
@@ -1,8 +1,21 @@
 from __future__ import absolute_import, print_function, unicode_literals
 from cavorite.HTML import *
-
+from cavorite import t
 
 def mainscreen_view():
     return div([
                  p("Fuel watch"),
+                 p([t("Choose fuel type-")] + [
+                   select([
+                     option({'value': 1}, 'Unleaded Petrol'),
+                     option({'value': 2}, 'Premium Unleaded'),
+                     option({'value': 4}, 'Diesel'),
+                     option({'value': 5}, 'LPG'),
+                     option({'value': 6}, '98 RON'),
+                     option({'value': 10}, 'E85'),
+                     option({'value': 11},' Brand diesel'),
+                   ]),
+                   p(''),
+                   html_button('Lookup'),
+                 ]),
                ])
